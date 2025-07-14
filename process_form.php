6cc39b7d9039258abb90580307b11900946e1a9a
moffylo <moffylolimited@gmail.com>
2025-07-14 11:00:22 +0100

    n

--- process_form.php
+++ process_form.php
@@ -1,9 +1,9 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-    
+
     $to = "admin@moffylo.com";
     $subject = "New Message from Website";
     $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
@@ -13,6 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Message sent successfully!";
     } else {
         echo "Failed to send the message.";
-    }
+    }
 }
 ?>
