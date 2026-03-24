da66503bf9ca32b65c838c46e8520291d213ff6f
moffylo <moffylolimited@gmail.com>
2026-03-24 14:24:35 +0100

    v

--- script.js
+++ script.js
@@ -101,16 +101,3 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
-
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
-
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
-
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-})
